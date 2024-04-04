c9e1972f20ea3e72d01f18addd46c45d65a18dd6
Niyati Bafna <niyatibafna13@gmail.com>
2024-04-04 15:38:50 -0400

    testing noisers

---
+++ noisers/test_noise.py
@@ -0,0 +1,18 @@
+'''This file is for testing out various noise classes'''
+from main import parse_noise_params, get_noisers, apply_noisers
+
+noise_specs = "phonological:theta_1=0.5,theta_2=0.2;character_level:script=devanagari,insert_theta=0.1,delete_theta=0.1,swap_theta=0.1"
+all_noise_params = parse_noise_params(noise_specs)
+print(f"Noise Parameters: {all_noise_params}")
+
+noiser_classes = get_noisers(all_noise_params)
+print(f"Noiser Classes: {noiser_classes}")
+
+inputs = ["The quick brown fox jumps over the lazy dog", "जल में रहकर मगर से बैर"]
+
+for input in inputs:
+    print(f"Input: {input}")
+    noised = apply_noisers(input, noiser_classes, verbose=True)
+    print(f"Noised: {noised}")
+    print()
+

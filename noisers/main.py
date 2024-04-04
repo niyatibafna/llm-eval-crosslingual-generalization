4b94301eaf9cb08233e5bee088cebf68ae948e19
Niyati Bafna <niyatibafna13@gmail.com>
2024-04-04 14:00:12 -0400

    Changing framework of noising

--- noisers/main.py
+++ noisers/main.py
@@ -4,17 +4,30 @@ NOISE_REGISTRY = {
     'phonological': PhonologicalNoiser,
 }
 
-def noiser_main(input, all_noise_params):
-    '''Apply noise to input according to noise_params
+def get_noisers(all_noise_params):
+    '''Initialize noisers with noise parameters
     Args:
         input: str, input text
         all_noise_params: dict, noise parameters, like {phonological: {theta_1: 0.5}}
     Returns:
-        str, noised text
+        
     '''
-    # Apply noise
+    # Initialize noisers
+    noise_classes = list()
     for noise_type, noise_params in all_noise_params.items():
         noiser = NOISE_REGISTRY[noise_type](noise_params)
-        input = noiser.apply_noise(input)
+        noise_classes.append(noiser)
     
+    return noise_classes
+
+def apply_noisers(input, noise_classes):
+    '''Apply noise to input
+    Args:
+        input: str, input text
+        noise_classes: list, list of noisers
+    Returns:
+        str, noised text
+    '''
+    for noiser in noise_classes:
+        input = noiser.apply_noise(input)
     return input

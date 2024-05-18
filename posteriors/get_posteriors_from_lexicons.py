9671a2aef99870f0fbc41e6d071523e5283199cf
Niyati Bafna <niyatibafna13@gmail.com>
2024-05-18 15:27:10 +0200

    skeleton posteriors

--- posteriors/get_posteriors_from_lexicons.py
+++ posteriors/get_posteriors_from_lexicons.py
@@ -16,7 +16,7 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append("../")
 from noisers.utils.get_functional_words import OUTDIR, closed_class_tags
-from noisers.utils.get_functional_words import outpath_paths as ud_wordlists_paths
+from noisers.utils.get_functional_words import output_paths_local as ud_wordlists_paths
 
 from get_lexicons import json_to_list_of_pairs
 
@@ -78,14 +78,44 @@ class Posterior:
         
         return count_func/total_func, count_content/total_content
     
-    def post_phon_noiser(self):
+    def post_phonological_noiser(self):
+        '''
+        Find theta_phon i.e. the proportion of character ngrams with a new middle character.
+        We do this in the following way:
+        Collect all character grams of length 3 in the source lexicon
+        1. For every src-tgt pair, see whether they have "high-enough" NED
+        2. If so, count insertions, deletions, and replacements in between identical characters
+
+        There are a number of issues with the above, but there are also the same problems with the original
+        noiser, so it's okay. Or at least, it's consistently bad.
+        '''
         pass
     
+    def post_morphological_noiser(self):
+        '''
+        Find theta_morph i.e. the proportion of the top k suffixes that are new.
+        We do this in the following way:
+        1. Find the top k suffixes in the target language (do this in the same way that we do it
+            in the noiser - i.e. list the top string suffixes and then find the top k in a language specific
+            way)
+        2. For every source word, sample a suffix. If the suffix is not in our collected list, move on.
+        3. If it is, then check if the target word has the same "stem". We do this by checking if the
+            src and tgt have some non-trivial shared prefix (let's say length 2 or 33% of the length of the
+            word). If they do, then we say that the stem is the same. We can also use NED,
+            but this might be less noisy.
+        4. In this case, check if the suffix is the same. If not, then we have a change.
+        5. Note that we make this count several times per suffix, and we finally take an average for a given
+            suffix to decide whether it was changed or not (i.e. we'll get something like 0.3 for a suffix)
+        6. We then take the top k suffixes and find the average of the proportion of times they were changed
+            to get theta_morph.
+        '''
+
+        pass
 
 hin_langs = ["bho", "mag", "mai", "hne", "awa"]
 
 for lang in hin_langs:
-    bil_lexicon_json_file = f"/export/b08/nbafna1/projects/llm-robustness-to-xlingual-noise/posteriors/lexicons/hin-{lang}.json"
+    bil_lexicon_json_file = f"/Users/work/Desktop/projects/llm-robustness-to-xlingual-noise/posteriors/lexicons/hin-{lang}.json"
     bil_lexicon = json_to_list_of_pairs(bil_lexicon_json_file)
     src_vocab = set()
     tgt_vocab = set()

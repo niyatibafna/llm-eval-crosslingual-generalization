d77ce67ab43f9374ca9a567ef5c77b6441f6e7d9
Niyati Bafna <niyatibafna13@gmail.com>
2024-05-29 23:55:00 -0400

    Not allowing empty src tgt pairs

--- posteriors/get_lexicons.py
+++ posteriors/get_lexicons.py
@@ -8,14 +8,21 @@ def json_to_list_of_pairs(bil_lexicon_json_file):
     with open(bil_lexicon_json_file) as f:
         bil_lexicon_dict = json.load(f)
     
+    punctuation = ".,;:?!-_()[]{}\"'`~@#$%^&*+=|\\<>/"
+    
     bil_lexicon = list()
     for src in bil_lexicon_dict:
         # Pick best target
         if src in bil_lexicon_dict[src]:
-            bil_lexicon.append((src, src))
+            src = src.lower().strip(punctuation)
+            if src:
+                bil_lexicon.append((src, src))
             continue
         tgt = max(bil_lexicon_dict[src], key=bil_lexicon_dict[src].get)
-        bil_lexicon.append((src, tgt))
+        src = src.lower().strip(punctuation)
+        tgt = tgt.lower().strip(punctuation)
+        if src and tgt:
+            bil_lexicon.append((src, tgt))
 
     return bil_lexicon
     

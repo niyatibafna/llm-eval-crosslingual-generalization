648bb1adc87bca83f6ad647dc6b6c2835fd84d92
Niyati Bafna <niyatibafna13@gmail.com>
2024-06-16 04:09:26 -0400

    BL construction

--- posteriors/get_lexicons.py
+++ posteriors/get_lexicons.py
@@ -18,11 +18,61 @@ def json_to_list_of_pairs(bil_lexicon_json_file):
             if src:
                 bil_lexicon.append((src, src))
             continue
-        tgt = max(bil_lexicon_dict[src], key=bil_lexicon_dict[src].get)
-        src = src.lower().strip(punctuation)
-        tgt = tgt.lower().strip(punctuation)
+        if isinstance(bil_lexicon_dict[src], str):
+            tgt = bil_lexicon_dict[src]
+        else:
+            tgt = max(bil_lexicon_dict[src], key=bil_lexicon_dict[src].get)
+            src = src.lower().strip(punctuation)
+            tgt = tgt.lower().strip(punctuation)
         if src and tgt:
             bil_lexicon.append((src, tgt))
 
     return bil_lexicon
+
+def google_translate_lexicon(bil_lexicon, src_lang, tgt_lang, outpath = None):
+    '''
+    Assuming that the structure of the original is 
+    src: {tgt1: freq1, tgt2: freq2, ...}
+    '''
+    import googletrans
+    print(f"Processing {src_lang} to {tgt_lang}...")
+    google_lexicon = dict()
+    for src, tgt in bil_lexicon:
+        tgt = googletrans.Translator().translate(src, src=src_lang, dest=tgt_lang).text
+        google_lexicon[src] = tgt
+        if len(google_lexicon) == 500:
+            break
+
+    if outpath:
+        with open(outpath, "w") as f:
+            json.dump(google_lexicon, f, indent=2, ensure_ascii=False)
     
+    return google_lexicon
+
+# bil_lexicon = "/export/b08/nbafna1/projects/llm-robustness-to-xlingual-noise/posteriors/flores_lexicons_raw/deu_swe.json"
+# src_lang = "de"
+# tgt_lang = "sv"
+# outpath = "/export/b08/nbafna1/projects/llm-robustness-to-xlingual-noise/posteriors/google_translate_lexicons/deu_swe.json"
+
+# google_translate_lexicon(json_to_list_of_pairs(bil_lexicon), src_lang, tgt_lang, outpath)
+
+# bil_lexicon = "/export/b08/nbafna1/projects/llm-robustness-to-xlingual-noise/posteriors/flores_lexicons_raw/deu_dan.json"
+# src_lang = "de"
+# tgt_lang = "da"
+# outpath = "/export/b08/nbafna1/projects/llm-robustness-to-xlingual-noise/posteriors/google_translate_lexicons/deu_dan.json"
+
+# google_translate_lexicon(json_to_list_of_pairs(bil_lexicon), src_lang, tgt_lang, outpath)
+
+# bil_lexicon = "/export/b08/nbafna1/projects/llm-robustness-to-xlingual-noise/posteriors/flores_lexicons_raw/deu_isl.json"
+# src_lang = "de"
+# tgt_lang = "is"
+# outpath = "/export/b08/nbafna1/projects/llm-robustness-to-xlingual-noise/posteriors/google_translate_lexicons/deu_isl.json"
+
+# google_translate_lexicon(json_to_list_of_pairs(bil_lexicon), src_lang, tgt_lang, outpath)
+
+# bil_lexicon = "/export/b08/nbafna1/projects/llm-robustness-to-xlingual-noise/posteriors/flores_lexicons_raw/ind_zsm.json"
+# src_lang = "id"
+# tgt_lang = "ms"
+# outpath = "/export/b08/nbafna1/projects/llm-robustness-to-xlingual-noise/posteriors/google_translate_lexicons/ind_zsm.json"
+
+# google_translate_lexicon(json_to_list_of_pairs(bil_lexicon), src_lang, tgt_lang, outpath)
